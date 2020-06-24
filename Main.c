43fc484de0f7180f2080cac888f85a825b276d1b
fabienfors <67374489+fabienfors@users.noreply.github.com>
2020-06-24 16:04:07 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,4 +1,29 @@
-main()
+int main()
 {
+/*name: C/C++ CI
 
+on:
+  push:
+    branches: [ master ]
+  pull_request:
+    branches: [ master ]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: configure
+      run: ./configure
+    - name: make
+      run: make
+    - name: make check
+      run: make check
+    - name: make distcheck
+      run: make distcheck
+ */
+  cout<<"Hello World"<<endl;
+  return 0;
 }
